6407594eef8363ab2ca4d1bb9ff12c4b8cb5fcb6
Scrlkk <shevaazki6@gmail.com>
2024-11-24 23:08:54 +0700

    Drinks

---
+++ resources/js/Components/DrinkContent.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+    drink: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <!-- CARD -->
+    <div
+        class="group w-[317px] hover:scale-105 transition duration-200 ease-in-out"
+    >
+        <!-- IMAGE -->
+        <div class="w-[317px] h-[180px] rounded-t-xl overflow-hidden">
+            <img
+                :src="drink.img"
+                alt="kebab"
+                class="object-cover w-full h-full"
+            />
+        </div>
+
+        <!-- DESC -->
+        <div
+            class="p-5 px-6 overflow-hidden bg-gray-200 rounded-b-xl group-hover:bg-red-600 group-hover:text-white transition duration-200 ease-in-out"
+        >
+            <h1 class="text-[23px] pb-2 font-bold font-raleway">
+                {{ drink.title }}
+            </h1>
+            <p class="h-[6rem] tracking-tight">
+                {{ drink.description }}
+            </p>
+            <h2
+                class="font-bold tracking-tight font-montserrat text-xl text-red-600 group-hover:text-white transition duration-200 ease-in-out"
+            >
+                Rp. {{ drink.price }}
+            </h2>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/MenuViews.vue
+++ resources/js/Pages/MenuViews.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import { ref } from "vue";
-import CardContent from "@/Components/CardContent.vue";
+import FoodContent from "@/Components/FoodContent.vue";
+import DrinkContent from "@/Components/DrinkContent.vue";
 import MainLayouts from "@/Layouts/MainLayouts.vue";
 import TersediaContent from "@/Components/TersediaContent.vue";
 
@@ -10,35 +11,12 @@ import Kebab2 from "@/Assets/card/kebab2.jpg";
 import Kebab3 from "@/Assets/card/kebab3.jpg";
 import Kebab4 from "@/Assets/card/kebab4.jpg";
 
-const Cards = ref([
-    {
-        img: Kebab1,
-        title: "Kebab Muwani Gilani",
-        description:
-            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
-        price: "20.000",
-    },
-    {
-        img: Kebab2,
-        title: "Kebab Mas Rehan",
-        description:
-            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
-        price: "17.000",
-    },
-    {
-        img: Kebab3,
-        title: "Kebab Ambaffi Wuenak",
-        description:
-            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
-        price: "30.000",
-    },
-    {
-        img: Kebab4,
-        title: "Kebab Ngawi blukutuk",
-        description:
-            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
-        price: "50.000",
-    },
+import Drinks1 from "@/Assets/drinks/esteh.jpg";
+import Drinks2 from "@/Assets/drinks/esjeruk.jpg";
+import Drinks3 from "@/Assets/drinks/eslemon.jpg";
+import Drinks4 from "@/Assets/drinks/boba1.jpg";
+
+const Foods = ref([
     {
         img: Kebab1,
         title: "Kebab Muwani Gilani",
@@ -97,6 +75,37 @@ const Cards = ref([
     },
 ]);
 
+const Drinks = ref([
+    {
+        img: Drinks1,
+        title: "Es Teh",
+        description:
+            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
+        price: "3.000",
+    },
+    {
+        img: Drinks2,
+        title: "Es Jeruk",
+        description:
+            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
+        price: "4.000",
+    },
+    {
+        img: Drinks3,
+        title: "Es Lemon",
+        description:
+            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
+        price: "5.000",
+    },
+    {
+        img: Drinks4,
+        title: "Booba",
+        description:
+            "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes auctor sagittis hac torquent a nullam mi platea.",
+        price: "15.000",
+    },
+]);
+
 const getCardsByRange = (start, end) => Cards.value.slice(start, end);
 </script>
 
@@ -114,25 +123,52 @@ const getCardsByRange = (start, end) => Cards.value.slice(start, end);
                     />
                 </div>
                 <div
-                    class="absolute flex inset-0 items-center justify-center w-full font-raleway text-6xl text-white font-bold"
+                    class="absolute flex inset-0 items-center justify-center w-full font-raleway text-[5rem] text-white font-bold"
                 >
                     OUR MENU
                 </div>
             </div>
         </section>
 
-        <!-- TITLE -->
+        <!-- RED -->
         <section>
             <div class="bg-red-600 py-3"></div>
         </section>
 
-        <!-- Content -->
-        <section class="px-24 pt-32 pb-40">
+        <!-- Title -->
+        <section>
+            <div class="text-center text-3xl font-montserrat pt-28 pb-12">
+                Menu
+                <span class="text-red-600 font-bold"> Kebab </span>
+            </div>
+        </section>
+
+        <!-- Kebab -->
+        <section class="px-24 pt-12 pb-10">
             <div class="grid grid-cols-4 gap-12">
-                <CardContent
-                    v-for="(card, index) in Cards"
+                <FoodContent
+                    v-for="(food, index) in Foods"
                     :key="index"
-                    :card="card"
+                    :food="food"
+                    loading="lazy"
+                />
+            </div>
+        </section>
+
+        <!-- Title -->
+        <section>
+            <div class="text-center text-3xl font-montserrat pt-28 pb-12">
+                Menu
+                <span class="text-red-600 font-bold"> Minuman </span>
+            </div>
+        </section>
+        <!-- Drinks -->
+        <section class="px-24 pb-40">
+            <div class="grid grid-cols-4 gap-12">
+                <DrinkContent
+                    v-for="(drink, index) in Drinks"
+                    :key="index"
+                    :drink="drink"
                     loading="lazy"
                 />
             </div>
