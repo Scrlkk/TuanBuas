b116ceb7e8c9675b7603929f3671fe16e5c65344
Scrlkk <shevaazki6@gmail.com>
2024-12-02 03:35:01 +0700

    Deploy rev rev

--- public/index.php
+++ public/index.php
@@ -1,17 +1,30 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app->make(Illuminate\Contracts\Http\Kernel::class)
+    ->handle(Request::capture());
+
+$host = '0.0.0.0';
+$port = env('PORT', 8080);  // Ambil nilai PORT dari Railway, default ke 8080 jika tidak ada
+
+// Menggunakan Laravel built-in server (jika perlu)
+if (env('APP_ENV') === 'local') {
+    Artisan::call('serve', [
+        '--host' => $host,
+        '--port' => $port
+    ]);
+}
